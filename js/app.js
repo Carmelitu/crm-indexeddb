a773c4a49981e90b1cdd545934ef54e584589ae4
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-07 22:25:17 -0300

    se elimina cliente correctamente, proyecto finalizado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function() {
 
     let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
@@ -8,7 +9,33 @@
         if(window.indexedDB.open('crm', 1)){
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener('click', eliminarRegistro);
     });
+
+    function eliminarRegistro(e){
+        if (e.target.classList.contains('eliminar')){
+            const idEliminar = Number(e.target.dataset.cliente);
+            const confirmar = confirm('Deseas eliminar este cliente?');
+
+            if (confirmar){
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = () => {
+                    console.log('Cliente eliminado correctamente');
+
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = () => {
+                    console.log('Hubo un error', 'error');
+                }
+            }
+        }
+    }
 
 
     // Crea base de datos IndexedDB
@@ -56,7 +83,6 @@
                 if (cursor){
                     const {id, nombre, email, telefono, empresa} = cursor.value;
 
-                    const listadoClientes = document.querySelector('#listado-clientes');
                     listadoClientes.innerHTML += `
                         <tr>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
@@ -71,14 +97,12 @@
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                             </td>
                         </tr>
                     `;
 
                     cursor.continue();
-                } else {
-                    console.log('No hay mas registros');
                 }
             }
         }
