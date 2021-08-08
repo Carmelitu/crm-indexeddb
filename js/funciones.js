c9054b1a54a245de370a9a1c5436b93bb064463d
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-07 22:27:54 -0300

    se agrega ultima funcion

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,30 @@
+function crearDB(){
+    var crearDB = window.indexedDB.open('crm', 1);
+
+    crearDB.onerror = () => {
+        console.log('Error al crear DB');
+    }
+
+    crearDB.onsuccess = () => {
+        DB = crearDB.result;            
+    }
+
+
+    crearDB.onupgradeneeded = (e) => {
+        const db = e.target.result;
+
+        const objectStore = db.createObjectStore('crm', {keyPath: 'id', autoIncrement: true});
+
+        objectStore.createIndex('nombre', 'nombre', {unique: false});
+        objectStore.createIndex('email', 'email', {unique: true});
+        objectStore.createIndex('telefono', 'telefono', {unique: false});
+        objectStore.createIndex('empresa', 'empresa', {unique: false});
+        objectStore.createIndex('id', 'id', {unique: true});
+
+        console.log('DB Lista y creada');
+    }
+}
+
 function conectarDB(){
     const abrirConexion = window.indexedDB.open('crm', 1);
 
